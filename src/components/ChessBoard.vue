9883c83bbe6eab09e11e41d653222151310c5109
Benjamin Eyzaguirre <caxco93@gmail.com>
2023-05-05 16:07:18 -0500

    Prettier setup (#3)
    * Add prettier dev dependencies
    
    * Add prettier to eslint config file
    
    - This allows eslint to use prettier as a rule while simultaneously
    removing the need for ESLint to deal with formatting concerns
    
    * Run eslint --fix (now with prettier)

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,48 +1,42 @@
 <script setup lang="ts">
-
-const getSquareClass = (row: number, column:number) : string=>{
+const getSquareClass = (row: number, column: number): string => {
   return (row + column) % 2 === 0 ? "light" : "dark";
 };
 </script>
 
 <template>
   <div class="board">
-    <div
-      v-for="row in 8"
-      :key="row"
-      class="rank"
-    >
+    <div v-for="row in 8" :key="row" class="rank">
       <div
         v-for="column in 8"
         :key="column"
         class="square"
-        :class="getSquareClass(row,column)"
+        :class="getSquareClass(row, column)"
       />
     </div>
   </div>
 </template>
 
 <style scoped>
-.board{
+.board {
   max-width: 100vh;
   margin: auto;
   aspect-ratio: 1;
   display: flex;
   flex-direction: column;
 }
-.rank{
+.rank {
   display: flex;
-  flex:1;
+  flex: 1;
 }
-.square{
+.square {
   display: inline-block;
-  flex:1;
+  flex: 1;
 }
-.light{
+.light {
   background-color: #eeeed2;
-
 }
-.dark{
+.dark {
   background-color: #769656;
 }
 </style>
