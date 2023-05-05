3fe99ec0c887a0e5921a5590b986ee7afaf5dac3
Benjamin Eyzaguirre <caxco93@gmail.com>
2023-05-05 09:59:53 -0500

    Implement basic ChessBoard and SideBar (#2)
    * Create initial components
    
    * Remove unused CSS
    
    * Add basic Board/Sidebar layout
    
    * Make app element take all height
    
    * Add basic ChessBoard component
    
    * Add color variables
    
    * Use color variables in SideBar component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import ChessBoard from "./components/ChessBoard.vue";
+import SideBar from "./components/SideBar.vue";
 </script>
 
 <template>
-  <div>
-    <a
-      href="https://vitejs.dev"
-      target="_blank"
-    >
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
-      >
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-    >
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      >
-    </a>
+  <div class="app">
+    <main>
+      <ChessBoard />
+    </main>
+    <aside>
+      <SideBar />
+    </aside>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.app{
+  display:grid;
+  grid-template-columns: 1fr 300px;
+  height: 100vh;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="sidebar">
+    SideBar
+  </div>
+</template>
+
+<style scoped>
+.sidebar{
+  background-color: var(--dark-secondary);
+  height:100vh;
+}
+@media (prefers-color-scheme: light) {
+  .sidebar {
+    color: var(--dark-main);
+    background-color: var(--light-main);
+  }
+}
+</style>
