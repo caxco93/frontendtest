f8abe0d0d45191d9fbc307d6eabda8467368892b
caxco93 <caxco93@gmail.com>
2023-05-04 23:55:50 -0500

    Add basic ChessBoard component

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
+
+const getSquareClass = (row: number, column:number) : string=>{
+  return (row + column) % 2 === 0 ? "light" : "dark";
+};
 </script>
 
 <template>
-  <div>ChessBoard</div>
+  <div class="board">
+    <div
+      v-for="row in 8"
+      :key="row"
+      class="rank"
+    >
+      <div
+        v-for="column in 8"
+        :key="column"
+        class="square"
+        :class="getSquareClass(row,column)"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.board{
+  max-width: 100vh;
+  margin: auto;
+  aspect-ratio: 1;
+  display: flex;
+  flex-direction: column;
+}
+.rank{
+  display: flex;
+  flex:1;
+}
+.square{
+  display: inline-block;
+  flex:1;
+}
+.light{
+  background-color: #eeeed2;
+
+}
+.dark{
+  background-color: #769656;
+}
 </style>
