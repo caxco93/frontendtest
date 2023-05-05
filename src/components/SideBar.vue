6f9cd0f367cc5078ab031ad290ae0dc7fc23c0c9
caxco93 <caxco93@gmail.com>
2023-05-05 08:25:12 -0500

    Use color variables in SideBar component

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,8 +2,20 @@
 </script>
 
 <template>
-  <div>SideBar</div>
+  <div class="sidebar">
+    SideBar
+  </div>
 </template>
 
 <style scoped>
+.sidebar{
+  background-color: var(--dark-secondary);
+  height:100vh;
+}
+@media (prefers-color-scheme: light) {
+  .sidebar {
+    color: var(--dark-main);
+    background-color: var(--light-main);
+  }
+}
 </style>
