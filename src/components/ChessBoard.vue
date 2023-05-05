a135dbc89e72e28cc8d03cb3c149871ab8bb0e80
caxco93 <caxco93@gmail.com>
2023-05-05 17:04:16 -0500

    Add basic highlight logic idea

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -5,19 +5,27 @@ const getSquareClass = (row: number, column: number): string => {
 </script>
 
 <template>
-  <div class="board">
-    <template v-for="row in 8">
-      <div
-        v-for="column in 8"
-        :key="`${row}${column}`"
-        class="square"
-        :class="getSquareClass(row, column)"
-      />
-    </template>
+  <div class="wrapper">
+    <div class="board">
+      <template v-for="row in 8">
+        <div
+          v-for="column in 8"
+          :key="`${row}${column}`"
+          class="square"
+          :class="getSquareClass(row, column)"
+        />
+      </template>
+    </div>
+    <div class="board highlight">
+      <div class="square"></div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.wrapper {
+  position: relative;
+}
 .board {
   max-width: 100vh;
   margin: auto;
@@ -25,6 +33,19 @@ const getSquareClass = (row: number, column: number): string => {
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   grid-template-rows: repeat(8, 1fr);
+}
+.highlight {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.highlight .square {
+  grid-column-start: 4;
+  grid-row-start: 4;
+  background-color: rgb(235, 97, 80);
+  opacity: 0.8;
 }
 .light {
   background-color: #eeeed2;
