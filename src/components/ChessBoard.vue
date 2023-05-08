70e41cb03cb8fce085d094f7fdded00060d33995
Benjamin Eyzaguirre <caxco93@gmail.com>
2023-05-08 00:20:28 -0500

    Set correct File Letter / Rank Number positioning (#5)
    * Revert ChessBoard back to display flex.
    
    - This allows the board to be much more lean logic wise.
    - The chessboard in theory could even be an image, (just like Chess.com) does.
    However, if we do this, then we would have to create a separate way of handling
    the click events. I thought about 2 different ways to do so:
    
    The first one would be to track clicks on the image element itself, which would
    require us to use the click position to compute the corresponding square coordinate.
    This seems really error prone so I would definitely avoid that.
    
    The second one would be to create an overlayed div layer on top of the image with
    div cells arranged in a grid fashion. This is more viable but I'm still not a fan of
    separating the board display from the click handlers, as they could easily
    get misaligned, leading to bugs.
    
    For these reasons I prefer to handle both the drawing and the clicking together.
    
    * Install sass and sass-loader
    
    * Handle square color entirely by CSS
    
    * Create named grid template areas
    
    * Use named grid area for highlight position
    
    * Add rudimentary square click log visualization

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,36 +1,54 @@
 <script setup lang="ts">
 import { store } from "@/store.ts";
-import { Coordinate } from "@/types";
-const getSquareClass = (row: number, column: number): string => {
-  return (row + column) % 2 === 0 ? "light" : "dark";
-};
-function handleSquareClick(row: number, column: number) {
-  const coordinate: Coordinate = { file: column, rank: row };
+import { Coordinate, FileLetter } from "@/types";
+
+function handleSquareClick(file: FileLetter, rank: number) {
+  const coordinate: Coordinate = { file, rank };
   store.addHighlight(coordinate);
 }
+const files: Array<FileLetter> = ["a", "b", "c", "d", "e", "f", "g", "h"];
 </script>
 
 <template>
   <div class="board">
-    <template v-for="row in 8">
+    <div v-for="rank in 8" :key="rank" class="rank">
       <div
-        v-for="column in 8"
-        :key="`${row}${column}`"
+        v-for="file in files"
+        :key="file"
         class="square"
-        :class="getSquareClass(row, column)"
-        @click="handleSquareClick(row, column)"
+        @click="handleSquareClick(file, rank)"
       />
-    </template>
+    </div>
   </div>
 </template>
 
-<style scoped>
-@import "@/styles/board.css";
-
-.light {
-  background-color: #eeeed2;
+<style scoped lang="scss">
+.board {
+  max-width: 100vh;
+  margin: auto;
+  aspect-ratio: 1;
+  display: flex;
+  flex-direction: column-reverse;
 }
-.dark {
-  background-color: #769656;
+
+.rank {
+  display: flex;
+  flex: 1;
+
+  .square {
+    flex: 1;
+    background-color: #769656;
+    &:nth-child(odd) {
+      background-color: #eeeed2;
+    }
+  }
+}
+.rank:nth-child(odd) {
+  .square {
+    background-color: #eeeed2;
+    &:nth-child(odd) {
+      background-color: #769656;
+    }
+  }
 }
 </style>
